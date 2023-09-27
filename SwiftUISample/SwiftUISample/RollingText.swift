cd7fdd59ca61ba80da453703a2035ec461f28503
jun.morita <jun.morita@gree.net>
2023-09-27 14:51:56 +0900

    add print

--- SwiftUISample/SwiftUISample/RollingText.swift
+++ SwiftUISample/SwiftUISample/RollingText.swift
@@ -14,6 +14,7 @@ struct RollingText: View {
     var body: some View {
         HStack(spacing: 0) {
             ForEach(0..<animationRange.count, id: \.self) { index in
+                let _ = print("index is \(index)")
                 Text("8")
                     .font(.largeTitle)
                     .fontWeight(.regular)
@@ -23,6 +24,8 @@ struct RollingText: View {
                             let size = proxy.size
                             VStack(spacing: 0) {
                                 ForEach(0...9, id: \.self) { number in
+                                    let _ = print("number is \(number)")
+                                    let _ = print("height is \(size.height)")
                                     Text("\(number)")
                                         .font(.largeTitle)
                                         .fontWeight(.regular)
@@ -37,6 +40,7 @@ struct RollingText: View {
         }
         .onAppear {
             animationRange = Array(repeating: 0, count: "\(newScore)".count)
+            let _ = print("first range is \(animationRange)")
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.06) {
                 updateText()
             }
@@ -51,9 +55,12 @@ struct RollingText: View {
     
     func updateText() {
         let stringValue = String(newScore)
+        print(stringValue)
         for (index, newScore) in zip(0..<stringValue.count, stringValue) {
+            let _ = print("for in index: \(index), newScore is \(newScore)")
             withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 1, blendDuration: 1)) {
                 animationRange[index] = (String(newScore) as NSString).integerValue
+                let _ = print("update range is \(animationRange)")
             }
         }
     }
